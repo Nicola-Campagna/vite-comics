3f7fea16ff526ce91c73f2008f6f1c020e79f650
Nicola-Campagna <campagnanicola379@gmail.com>
2023-02-28 15:46:47 +0100

    header

---
+++ src/App.vue
@@ -0,0 +1,22 @@
+<!-- Descrizione:
+Create un nuovo progetto utilizzando Vite e Vue 3 e definite i componenti necessari per strutturare il layout come da screenshot allegato.
+Quando la struttura a macroblocchi è pronta, popolate le voci di menu dinamicamente usando i data del componente.
+Per oggi diamo priorità alla struttura: quando è tutto bello solido, passiamo al Sass! -->
+
+<script>
+import AppHeader from './components/AppHeader.vue'
+export default {
+  components: {
+    AppHeader,
+  }
+}
+</script>
+
+<template>
+  <AppHeader />
+</template>
+
+<style lang="scss">
+@use "../src/scss/general.scss"
+</style>
+
---
+++ src/components/AppHeader.vue
@@ -0,0 +1,76 @@
+
+<script>
+
+export default {
+
+}
+
+</script>
+
+<template>
+  <header>
+    <div class="nav-head-container container">
+      <img src="../assets/img/dc-logo.png" alt="logo">
+      <ul>
+        <li>
+          <a href="#">CHARACTERS</a>
+        </li>
+        <li>
+          <a href="#">COMICS</a>
+        </li>
+        <li>
+          <a href="#">MOVIES</a>
+        </li>
+        <li>
+          <a href="#">TV</a>
+        </li>
+        <li>
+          <a href="#">GAMES</a>
+        </li>
+        <li>
+          <a href="#">COLLECTIBLES</a>
+        </li>
+        <li>
+          <a href="#">VIDEOS</a>
+        </li>
+        <li>
+          <a href="#">FANS</a>
+        </li>
+        <li>
+          <a href="#">NEWS</a>
+        </li>
+        <li>
+          <a href="#">SHOP</a>
+        </li>
+      </ul>
+
+    </div>
+
+  </header>
+</template>
+
+<style lang="scss" scoped>
+// @use "../scss/mixin.scss" as *;
+
+header .nav-head-container {
+  display: flex;
+  justify-content: space-around;
+  align-items: center;
+}
+
+header ul li {
+  display: inline-block;
+  padding: 0 10px;
+
+  a {
+    font-size: 12px;
+    text-decoration: none;
+    color: #000;
+
+    &:hover {
+      color: blue;
+    }
+  }
+
+}
+</style>
